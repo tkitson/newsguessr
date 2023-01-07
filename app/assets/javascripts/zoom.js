a993f1bd34c68cd1623a3af180f08c856008abd7
Tom Kitson <thomaspfkitson@gmail.com>
2023-01-07 16:08:33 +0000

    slightly improved zoom feature

---
+++ app/assets/javascripts/zoom.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+
+console.log(img);
+console.log(modalImg);
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function() {
+    img01.className += " out";
+    setTimeout(function() {
+       modal.style.display = "none";
+       img01.className = "modal-content";
+     }, 400);
+
+ }
