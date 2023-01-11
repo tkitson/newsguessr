5c6333ddc1d2eb997e88472ec690c29ffbb92a6d
Tom Kitson <thomaspfkitson@gmail.com>
2023-01-11 18:00:33 +0000

    responsive for mobile

--- app/javascript/controllers/glide.js
+++ app/javascript/controllers/glide.js
@@ -2,7 +2,16 @@ import Glide from "@glidejs/glide";
 
 new Glide('.glide', {
   perView: 1,
-  type: 'carousel',
+  type: 'slider',
+  focusAt: 'center',
+  // controls: 'controls',
+  // breakpoints: {
+  //   // under 800px override
+  //   800: {
+  //     autoplay: 2000,
+  //     perView: 1
+  //   }
+  // }
 }
 
 ).mount()
