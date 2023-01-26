687a46897f227410e9cc37afa6fa268b8e70421a
Tom Kitson <thomaspfkitson@gmail.com>
2023-01-26 10:17:31 +0000

    volumes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, { url: "redis://default:b669310ea6444d0282397b0b266a47c0@fly-dawn-hill-4368.upstash.io", expires_in: 90.minutes }
-
+  config.cache_store = :redis_cache_store, { url: "redis://default:4b1a70ad91d6490b8e7dc6280c9478f1@fly-white-waterfall-6812.upstash.io", expires_in: 90.minutes }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
