5daf1b9f0b446ab8d2e3d44e520401fcc958aa18
Tom Kitson <thomaspfkitson@gmail.com>
2023-03-08 16:14:47 +0000

    system test for displaying images

--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -1,19 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Home page", type: :system do
-  it "loads successfully" do
-    visit root_path
-    expect(page).to have_http_status(:ok)
-  end
 
   it "displays a form to make a new guess" do
     visit root_path
     expect(page).to have_css("form#new_user_guess")
   end
 
-  it "displays today's paper" do
-    live_paper = create(:newspaper, live: true)
+  it "displays images" do
+    page.all('#front-page-img').each do |img|
+      visit img[:src]
+      page.status_code.should be 200
+    end
+  end
+
+  it "Tells the user when the answer is incorrect" do
     visit root_path
-    expect(page).to have_css("img[src*='#{live_paper.image_url}']")
+    fill_in 'user_guess[date]', with: '1966'
+    click_button 'commit'
+    expect(page).to have_content("Incorrect")
   end
 end
